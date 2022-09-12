36474b92989bd1fcb2cb96138f9d4348cc6552b6
emirhan5445 <80173420+emirhan5445@users.noreply.github.com>
2022-09-13 01:03:11 +0300

    A few changes that are not necessary

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ function App() {
   return (
     <>
       <div className="online_3d_viewer" style={{width: "600px", height: "500px", border: "1px solid #000"}}
-        model="/models/arduino.step"
-        environmentmap="build/envmaps/fishermans_bastion/posx.jpg,build/envmaps/fishermans_bastion/negx.jpg,build/envmaps/fishermans_bastion/posy.jpg,build/envmaps/fishermans_bastion/negy.jpg,build/envmaps/fishermans_bastion/posz.jpg,build/envmaps/fishermans_bastion/negz.jpg">
+        model="/models/crystal.wrl">
       </div>
     </>
   );
